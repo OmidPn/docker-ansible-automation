fbe7dcfa4128a53f1d9dadb03da1c1ba6a7097ef
OmidPn <sdlc.canada@gmail.com>
2018-11-08 22:08:16 +0000

    chmod 777 for all branches

--- src/todobackend/settings.py
+++ src/todobackend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9@=5^0ws-^@*1uw4a$&(=l9dcwu&g9x$-m$5cg8p_%bjx$a=cz'
+SECRET_KEY = '#%8=y*l-+z*+mot(0+17@lm%!_yt4zb*j69fag-x((vz^zif(l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,10 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'todo'
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS Settings
+
+CORS_ORIGIN_ALLOW_ALL = True
